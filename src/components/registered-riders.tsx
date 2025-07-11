3b16c428740da2990f640f7d75d518963d01b025
COOL BOY DJ RATHAN <coolboydjrathan@gmail.com>
2025-07-11 09:57:46 +0000

    skeleton loader has 3 vertical element just add one its enough

--- src/components/registered-riders.tsx
+++ src/components/registered-riders.tsx
@@ -57,8 +57,6 @@ export function RegisteredRiders() {
            <CardContent>
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                   <RiderSkeleton />
-                  <RiderSkeleton />
-                  <RiderSkeleton />
               </div>
            </CardContent>
        </Card>
