8e788c485593af8e410d4faa6c1d9488989cdd60
COOL BOY DJ RATHAN <coolboydjrathan@gmail.com>
2025-08-09 18:43:10 +0000

    Still Space is there in Desktop site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,18 +31,25 @@ export default function Home() {
       <main className="flex-grow container mx-auto p-4 md:p-8 space-y-8">
         <Hero />
         <RegisteredRiders />
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          <div className="lg:col-span-2 space-y-8">
-             <EventSchedule />
-             <RouteMap />
-             <Faq />
-          </div>
-          <div className="flex flex-col gap-8">
+        
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+            <EventSchedule />
+            <RouteMap />
+        </div>
+        
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
             <Announcements />
             <Offers />
-            <QnaSection />
-            <StoreDetails />
-          </div>
+        </div>
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="lg:col-span-2">
+                <Faq />
+            </div>
+            <div className="md:col-span-2 lg:col-span-1 flex flex-col gap-8">
+                 <QnaSection />
+                 <StoreDetails />
+            </div>
         </div>
         
         <Organizers />
