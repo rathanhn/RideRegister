cb79c027c2946fd9bc56801180fc92f59709430d
COOL BOY DJ RATHAN <coolboydjrathan@gmail.com>
2025-08-09 20:06:49 +0000

    i want to specify the place while configuring APi key or what ?
    or you

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    instrumentationHook: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
