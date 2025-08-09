b669cc0d535152f7b119af2215ad08e9cefa02db
COOL BOY DJ RATHAN <coolboydjrathan@gmail.com>
2025-08-09 18:45:04 +0000

    Move that below the anouncement section
    now anouncement swction has lot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,9 +37,14 @@ export default function Home() {
             <RouteMap />
         </div>
         
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            <Announcements />
-            <Offers />
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-start">
+            <div className="space-y-8">
+              <Announcements />
+              <QnaSection />
+            </div>
+            <div className="space-y-8">
+              <Offers />
+            </div>
         </div>
         
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
@@ -47,7 +52,6 @@ export default function Home() {
                 <Faq />
             </div>
             <div className="md:col-span-2 lg:col-span-1 flex flex-col gap-8">
-                 <QnaSection />
                  <StoreDetails />
             </div>
         </div>
