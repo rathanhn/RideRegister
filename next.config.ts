9f674d123fc35e26935a8791ba8d0365b7ec5261
COOL BOY DJ RATHAN <coolboydjrathan@gmail.com>
2025-08-09 20:05:14 +0000

    But im not able to view Reviews
    it says
    Failed to Load Reviews
    
    Failed

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    instrumentationHook: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
